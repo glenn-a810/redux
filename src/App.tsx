91156ae8ab0f5a3fc07924224144cfa0a2a07b45
Glenn <a810.glenn@gmail.com>
2022-10-14 21:00:14 +0900

    useSelector, useDispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from './reducer'
 
 type Props = {
   value: any
@@ -8,6 +10,10 @@ type Props = {
 }
 
 function App({ value, onIncrement, onDecrement }: Props) {
+  const dispatch = useDispatch()
+  const counter = useSelector((state: RootState) => state.counter)
+  const todos: string[] = useSelector((state: RootState) => state.todos)
+
   const [todoValue, setTodoValue] = useState('')
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -16,14 +22,20 @@ function App({ value, onIncrement, onDecrement }: Props) {
 
   const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    dispatch({ type: 'ADD_TODO', text: todoValue })
     setTodoValue('')
   }
 
   return (
     <div className="App">
-      Clicked: {value} times
+      Clicked: {counter} times
       <button onClick={onIncrement}>+</button>{' '}
       <button onClick={onDecrement}>-</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
       <form onSubmit={addTodo}>
         <input type="text" value={todoValue} onChange={handleChange} />
         <input type="submit" />
--- src/index.tsx
+++ src/index.tsx
@@ -11,12 +11,6 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 const store = createStore(rootReducer)
 
-store.dispatch({
-  type: 'ADD_TODO',
-  text: 'USE REDUX',
-})
-console.log('store.getState()', store.getState())
-
 const render = () =>
   root.render(
     <React.StrictMode>
